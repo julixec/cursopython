d89f29981d9ba5868f7b5f5b38df211b608d3b1a
julixec <130595242+julixec@users.noreply.github.com>
2023-04-12 13:07:55 -0500

    Add files via upload

---
+++ class1-2 testjgalindo.py
@@ -0,0 +1,22 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5+5
+>>> [DEBUG ON]
+>>> [DEBUG OFF]
+>>> 
+>>> 5+5
+10
+>>> 5*2
+10
+>>> 10-1
+9
+>>> 10/2
+5.0
+>>> 20%4
+0
+>>> print(Longly Python)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("Longly Python")
+Longly Python
+>>> print("ok si funka ;)")
+ok si funka ;)
