03568f4f8eda2a104c1b0264a193a4eecbd51d0d
julixec <130595242+julixec@users.noreply.github.com>
2023-04-12 13:00:17 -0500

    Add files via upload

---
+++ class2 operdores.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+
+# Datos de ejemplo
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Crear el gráfico
+plt.plot(x, y)
+
+# Agregar etiquetas y título
+plt.xlabel('Eje X')
+plt.ylabel('Eje Y')
+plt.title('Mi Gráfico')
+
+# Mostrar el gráfico
+plt.show()
+#Este código creará un gráfico con los valores x en el eje horizontal y los valores y en el eje vertical. La función plot() traza los puntos en el gráfico y show() muestra el
+
+
+
+
+
+
+###class 2
+a=5+5
+print(a)
+
+
+print("Hola como cava ??? {}".format(a))
+print("que onda"+str(a))
+print(type(3.5))
+
+pi=22/7
+print(pi)
+
+# Code cell 5
+d = {'k1':['val1','val2','val3',{'we':['need','to','go',{'deeper':[1,2,3,'that']}]}]}
+that=d['k1'][3]['we'][3]['deeper'][3]
+print(that)
+
+
+
+
+
+
+
+
+
